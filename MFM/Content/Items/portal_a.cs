d28a7a35639e19cb5c57b64896a45087cfb3c75d
8105205-ai <8105205@gmail.com>
2025-12-12 20:36:59 +0300

    Add files via upload

---
+++ MFM/Content/Items/portal_a.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MFM.Content.Items
+{
+    public class portal_a : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.portal_a>());
+
+            Item.width = 40;
+            Item.height = 40;
+            Item.maxStack = 9999;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+        }
+    }
+}
