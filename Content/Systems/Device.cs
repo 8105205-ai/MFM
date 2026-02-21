17145d09e1918c48b5050b082c6507c21c830e9d
8105205-ai <8105205@gmail.com>
2026-02-21 12:54:45 +0300

    ну я вроде пофиксил алгоритм поиска связанных устройств, ну и ещё их теперб можно дисконнектить вот

--- Content/Systems/Device.cs
+++ Content/Systems/Device.cs
@@ -24,23 +24,24 @@ namespace MFM.Content.Systems
         public void Connect(Device device)
         {
             if (connected == null || !connected.Contains(device)) connected.Add(device);
+            else if (connected.Contains(device)) connected.Remove(device);
         }
 
-        public void Disconnect(Device device)
-        {
-            connected.Remove(device);
-        }
+        //public void Disconnect(Device device)
+        //{
+        //    connected.Remove(device);
+        //}
 
         public List<Device> allConected(List<Device> visited = null)
         {
             if (connected == null || connected.Count == 0) return new List<Device> {this};
             List<Device> result = new List<Device>();
-            if (visited == null) visited = new List<Device>();
+            result.Add(this);
             foreach (Device device in connected)
             {
-                if (visited.Contains(device)) continue;
+                if (visited != null && visited.Contains(device)) continue;
                 visited.Add(device);
-                result.AddRange(device.allConected(visited));
+                result.AddRange(device.allConected(result.Union(visited).ToList()));
             }
             return result;
         }
