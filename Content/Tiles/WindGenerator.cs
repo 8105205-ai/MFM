bc47a54fb09334f297f17e738f3c42695200dcce
8105205-ai <8105205@gmail.com>
2026-02-28 12:33:11 +0300

    nu vot tak

---
+++ Content/Tiles/WindGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MFM.Content.Tiles
+{
+    public class WindGenerator : BasicDevice
+    {
+        float windSpeed = Math.abs(Main.windSpeedCurrent)/0.8f; // hz rabotaet li tak esli cho peredelau, ya hz prost schas ne mogu zatestit
+        base.SetStaticDefaults();
+        power = 10;
+            DustType = DustID.Stone;
+            AddMapEntry(new Color(200, 200, 200));
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.addTile(Type);
+            TileID.Sets.HasOutlines[Type] = true;
+    }
+}
